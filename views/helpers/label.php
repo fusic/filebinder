feed3a808cce0e5d9633779d5b40d8e41e18090e
k1LoW <k1lowxb@gmail.com>
2010-07-02 10:02:53 +0900

    Modified LabelHelper

--- views/helpers/label.php
+++ views/helpers/label.php
@@ -10,10 +10,11 @@ class LabelHelper extends AppHelper {
      * @return
      */
     function image($file = null, $noImage = null){
-        if (empty($file['file_path']) || !preg_match('#' . WWW_ROOT . '#', $file['file_path'])) {
+        $filePath = empty($file['file_path']) ? (empty($file['tmp_bind_path']) ? false : $file['tmp_bind_path']) : $file['file_path'];
+        if (!$filePath || !preg_match('#' . WWW_ROOT . '#', $filePath)) {
             return $noImage;
         }
-        $src = preg_replace('#' . WWW_ROOT . '#', '../', $file['file_path']);
+        $src = preg_replace('#' . WWW_ROOT . '#', '../', $filePath);
         return $this->Html->image($src);
     }
 
