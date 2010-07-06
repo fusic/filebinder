3b764f63966c03cb228ddda5a7f356f80489c93e
k1LoW <k1lowxb@gmail.com>
2010-07-06 17:56:20 +0900

    Update LabelHelper::image

--- views/helpers/label.php
+++ views/helpers/label.php
@@ -9,13 +9,14 @@ class LabelHelper extends AppHelper {
      * @param $file
      * @return
      */
-    function image($file = null, $noImage = null){
+    function image($file = null, $options = array()){
         $filePath = empty($file['file_path']) ? (empty($file['tmp_bind_path']) ? false : $file['tmp_bind_path']) : $file['file_path'];
         if (!$filePath || !preg_match('#' . WWW_ROOT . '#', $filePath)) {
-            return $noImage;
+            return empty($options['noImage']) ? '' : $options['noImage'];
         }
         $src = preg_replace('#' . WWW_ROOT . '#', '../', $filePath);
-        return $this->Html->image($src);
+        unset($options['noImage']);
+        return $this->Html->image($src, $options);
     }
 
   }
