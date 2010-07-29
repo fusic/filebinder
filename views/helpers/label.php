527048e79155f5468ea51f5ed913c346a4d53ba5
k1LoW <k1lowxb@gmail.com>
2010-07-29 19:51:56 +0900

    Improved: image file link

--- views/helpers/label.php
+++ views/helpers/label.php
@@ -15,19 +15,19 @@ class LabelHelper extends AppHelper {
         if (!$filePath) {
             return empty($options['noFile']) ? '' : $options['noFile'];
         }
-        unset($options['noFile']);
         if (!preg_match('#' . WWW_ROOT . '#', $filePath)) {
             return $this->Html->image(array('admin' => false,
-                                                               'plugin' => 'filebinder',
-                                                               'controller' => 'filebinder',
-                                                               'action' => 'loader',
-                                                               $file['model'],
-                                                               $file['model_id'],
-                                                               $file['field_name'],
-                                                               Security::hash($file['model'] . $file['model_id'] . $file['field_name'] . $hash),
-                                                               $file['file_name']), $options);
+                                            'plugin' => 'filebinder',
+                                            'controller' => 'filebinder',
+                                            'action' => 'loader',
+                                            $file['model'],
+                                            $file['model_id'],
+                                            $file['field_name'],
+                                            Security::hash($file['model'] . $file['model_id'] . $file['field_name'] . $hash),
+                                            $file['file_name']), $options);
         }
         $src = preg_replace('#' . WWW_ROOT . '#', '../', $filePath);
+        unset($options['noImage']);
         return $this->Html->image($src, $options);
     }
 
@@ -40,20 +40,21 @@ class LabelHelper extends AppHelper {
     function link($file = null, $options = array()){
         $hash = $this->Session->read('Filebinder.hash');
         $filePath = empty($file['file_path']) ? (empty($file['tmp_bind_path']) ? false : $file['tmp_bind_path']) : $file['file_path'];
+        $fileTitle = empty($options['title']) ? $file['file_name'] : $options['title'];
+        unset($options['title']);
         if (!$filePath) {
             return empty($options['noFile']) ? '' : $options['noFile'];
         }
-        unset($options['noFile']);
         if (!preg_match('#' . WWW_ROOT . '#', $filePath)) {
-            return $this->Html->link($file['file_name'], array('admin' => false,
-                                                               'plugin' => 'filebinder',
-                                                               'controller' => 'filebinder',
-                                                               'action' => 'loader',
-                                                               $file['model'],
-                                                               $file['model_id'],
-                                                               $file['field_name'],
-                                                               Security::hash($file['model'] . $file['model_id'] . $file['field_name'] . $hash),
-                                                               $file['file_name']));
+            return $this->Html->link($fileTitle, array('admin' => false,
+                                                       'plugin' => 'filebinder',
+                                                       'controller' => 'filebinder',
+                                                       'action' => 'loader',
+                                                       $file['model'],
+                                                       $file['model_id'],
+                                                       $file['field_name'],
+                                                       Security::hash($file['model'] . $file['model_id'] . $file['field_name'] . $hash),
+                                                       $file['file_name']), $options);
         }
         $src = preg_replace('#' . WWW_ROOT . '#', '../', $filePath);
         return $this->Html->link($file['file_name'], $src);
