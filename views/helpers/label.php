8acce4f8ce48c3c3b6cd5979fcf466496d17b76a
k1LoW <k1lowxb@gmail.com>
2010-12-03 12:00:26 +0900

    Add LabelHelper::url(). Refactor code.

--- views/helpers/label.php
+++ views/helpers/label.php
@@ -10,26 +10,10 @@ class LabelHelper extends AppHelper {
      * @return
      */
     function image($file = null, $options = array()){
-        $hash = $this->Session->read('Filebinder.hash');
-        $prefix = empty($options['prefix']) ? '' : $options['prefix'];
-        $filePath = empty($file['file_path']) ? (empty($file['tmp_bind_path']) ? false : $file['tmp_bind_path']) : preg_replace('#/([^/]+)$#' , '/' . $prefix . '$1' , $file['file_path']);
-        if (!$filePath) {
+        if (!$this->_makeSrc($file, $options)) {
             return empty($options['noFile']) ? '' : $options['noFile'];
         }
-        if (!preg_match('#' . WWW_ROOT . '#', $filePath)) {
-            return $this->Html->image(array('admin' => false,
-                                            'plugin' => 'filebinder',
-                                            'controller' => 'filebinder',
-                                            'action' => 'loader',
-                                            $file['model'],
-                                            $file['model_id'],
-                                            $file['field_name'],
-                                            Security::hash($file['model'] . $file['model_id'] . $file['field_name'] . $hash),
-                                            $prefix . $file['file_name']), $options);
-        }
-        $src = preg_replace('#' . WWW_ROOT . '#', '../', $filePath);
-        unset($options['noImage']);
-        return $this->Html->image($src, $options);
+        return $this->Html->image($this->_makeSrc($file, $options), $options);
     }
 
     /**
@@ -39,27 +23,55 @@ class LabelHelper extends AppHelper {
      * @return
      */
     function link($file = null, $options = array()){
+        if (!$this->_makeSrc($file, $options)) {
+            return empty($options['noFile']) ? '' : $options['noFile'];
+        }
+        $fileTitle = empty($options['title']) ? $file['file_name'] : $options['title'];
+        unset($options['title']);
+        return $this->Html->link($fileTitle, $this->_makeSrc($file, $options), $options);
+    }
+
+    /**
+     * url
+     *
+     * @param
+     * @return
+     */
+    function url($file = null, $options = array()){
+        if (!$this->_makeSrc($file, $options)) {
+            return empty($options['noFile']) ? '' : $options['noFile'];
+        }
+        return $this->Html->url($this->_makeSrc($file, $options), $options);
+    }
+
+    /**
+     * _makeSrc
+     *
+     * @param $file
+     * @param $options
+     * @return
+     */
+    function _makeSrc($file = null, $options = array()){
         $hash = $this->Session->read('Filebinder.hash');
         $prefix = empty($options['prefix']) ? '' : $options['prefix'];
         $filePath = empty($file['file_path']) ? (empty($file['tmp_bind_path']) ? false : $file['tmp_bind_path']) : preg_replace('#/([^/]+)$#' , '/' . $prefix . '$1' , $file['file_path']);
-        $fileTitle = empty($options['title']) ? $file['file_name'] : $options['title'];
-        unset($options['title']);
         if (!$filePath) {
-            return empty($options['noFile']) ? '' : $options['noFile'];
+            return false;
         }
         if (!preg_match('#' . WWW_ROOT . '#', $filePath)) {
-            return $this->Html->link($fileTitle, array('admin' => false,
-                                                       'plugin' => 'filebinder',
-                                                       'controller' => 'filebinder',
-                                                       'action' => 'loader',
-                                                       $file['model'],
-                                                       $file['model_id'],
-                                                       $file['field_name'],
-                                                       Security::hash($file['model'] . $file['model_id'] . $file['field_name'] . $hash),
-                                                       $prefix . $file['file_name']), $options);
+            return array('admin' => false,
+                         'plugin' => 'filebinder',
+                         'controller' => 'filebinder',
+                         'action' => 'loader',
+                         $file['model'],
+                         $file['model_id'],
+                         $file['field_name'],
+                         Security::hash($file['model'] . $file['model_id'] . $file['field_name'] . $hash),
+                         $prefix . $file['file_name']);
         }
         $src = preg_replace('#' . WWW_ROOT . '#', '../', $filePath);
-        return $this->Html->link($file['file_name'], $src);
+        unset($options['noImage']);
+        return $src;
     }
 
   }
