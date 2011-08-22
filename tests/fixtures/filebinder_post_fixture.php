11794c857538bf11d61678384973e78a8a9ec0b9
k1LoW <k1lowxb@gmail.com>
2011-08-22 18:29:07 +0900

    Modify FilebinderPostFixture

--- tests/fixtures/filebinder_post_fixture.php
+++ tests/fixtures/filebinder_post_fixture.php
@@ -4,6 +4,8 @@ class FilebinderPostFixture extends CakeTestFixture {
 
     var $fields = array(
         'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 20, 'key' => 'primary'),
+        'title' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+        'body' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
         'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
         'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
     );
