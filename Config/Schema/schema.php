de43962a46d17d007b02df187e7a1d2f7ab911b1
k1LoW <k1lowxb@gmail.com>
2012-07-27 15:14:56 +0900

    Add .travis.yml

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -1,26 +1,11 @@
-<?php
-class attachementsSchema extends CakeSchema {
-    public $name = 'attachements';
+<?php 
+class FilebinderSchema extends CakeSchema {
 
-    public function before($event = array()) {
-        return true;
-    }
+	public function before($event = array()) {
+		return true;
+	}
 
-    public function after($event = array()) {
-    }
+	public function after($event = array()) {
+	}
 
-    public $attachments = array(
-                           'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
-                           'model' => array('type' => 'text', 'null' => false, 'default' => NULL),
-                           'model_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
-                           'field_name' => array('type' => 'text', 'null' => false, 'default' => NULL),
-                           'file_name' => array('type' => 'text', 'null' => false, 'default' => NULL),
-                           'file_content_type' => array('type' => 'text', 'null' => false, 'default' => NULL),
-                           'file_size' => array('type' => 'integer', 'null' => false, 'default' => NULL),
-                           'file_object' => array('type' => 'longtext', 'null' => true, 'default' => NULL), // error 'longtext' ...
-                           'created' => array('type' => 'timestamp', 'null' => true, 'default' => NULL),
-                           'modified' => array('type' => 'timestamp', 'null' => true, 'default' => NULL),
-                           'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
-                           'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
-                           );
-  }
+}
